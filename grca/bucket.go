8947897395e8582fd9038527bded40eacae75284
Hal Garland <28985585+HallyG@users.noreply.github.com>
2026-08-06 21:19:43 +0100

    Add simple grca rate limiter

---
+++ grca/bucket.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"time"
+)
+
+type GRCARateLimiter struct {
+	interval time.Duration
+	burst    int64
+
+	bucket map[string]time.Time
+	now    func() time.Time
+}
+
+func NewRateLimiter(rate float64, burst int64) *GRCARateLimiter {
+	return &GRCARateLimiter{
+		interval: time.Duration(float64(time.Second) / rate),
+		burst:    burst,
+		bucket:   make(map[string]time.Time),
+		now:      time.Now,
+	}
+}
+
+func (r *GRCARateLimiter) Allow(id string) bool {
+	now := time.Now()
+
+	tat, ok := r.bucket[id]
+	if !ok {
+		r.bucket[id] = now.Add(r.interval)
+		return true
+	}
+
+	limit := tat.Add(-time.Duration(r.burst) * r.interval)
+
+	if now.Before(limit) {
+		return false
+	}
+
+	// Advance the theoretical arrival time
+	if now.After(tat) {
+		tat = now
+	}
+
+	r.bucket[id] = tat.Add(r.interval)
+	return true
+}
